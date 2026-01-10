c9a402e4108e7c614400029ad9b261caa9328626
aaoganov <3145tttt002@mail.ru>
2026-01-11 00:23:49 +0300

    update req and save to dvc

--- save_dataset.py
+++ save_dataset.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     for data_type in ['train', 'test']:
         print(f"Saving data {data_type} ...")
         ds = get_data(SIZE, data_type)
-        ds.save_to_disk(f'./local_{SIZE}_{data_type}')
+        ds.save_to_disk(f'./datasets/{SIZE}_{data_type}')
