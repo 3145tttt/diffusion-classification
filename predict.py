655fcc166341787e781dbd837bdc063163d0107a
3145tttt <3145tttt002@mail.ru>
2026-01-11 15:13:14 +0300

    update predict logic

--- predict.py
+++ predict.py
@@ -19,7 +19,7 @@ def predict_img(model, img_tensor):
     probs = logits.softmax(0)
     pred_type = model.config.id2label[pred_id]
 
-    return  f"Pred diffusion = {pred_type}, with probability = {probs[pred_id].item():0.4f}"
+    return pred_type, probs[pred_id].item()
 
 @torch.inference_mode()
 def predict_arr(model, config, input_path, output_path):
@@ -31,15 +31,52 @@ def predict_arr(model, config, input_path, output_path):
     test_transform = create_base_transforms(config.base_size, image_processor, 'test')
 
     img_paths = sorted(os.listdir(input_path))
-    results = []
+    results_res = []
+    results_prob = []
     for img_path in tqdm(img_paths):
         img = Image.open(os.path.join(input_path, img_path)).convert('RGB')
         img_tensor = test_transform(img)[None]
-        res = predict_img(model, img_tensor)
-        results.append(res)
+        pred, prob = predict_img(model, img_tensor)
+        results_res.append(pred)
+        results_prob.append(prob)
 
-    df = pd.DataFrame({'paths': img_paths, 'output': results})
+    df = pd.DataFrame({'paths': img_paths, 'output': results_res, 'prob': results_prob})
     df.to_csv(output_path, index=False)
+
+
+@torch.inference_mode()
+def inference_model(
+    img,
+    target,
+    model,
+    config
+):
+    """
+    Inference model on test image
+    """
+    with open(config) as stream:
+        config = ConfigDict(yaml.safe_load(stream))
+
+    set_global_seed(config.seed)
+    _, image_processor = get_model_pretraned(config.model)
+
+    test_transform = create_base_transforms(config.base_size, image_processor, 'test')
+    img_tensor = test_transform(img)[None]
+    pred, prob = predict_img(model, img_tensor)
+    
+    print(f"Pred diffusion = {pred}, with probability = {prob:0.4f}")
+    print(f"True diffusion = {target}")
+
+
+if __name__ == "__main__":
+    test_object = load_from_disk('./datasets/ExtraSmall_test')[617]
+    repo_id = "3145tttt/diffusion-classification_base_resnet_50",
+    model = get_inference_model(repo_id)
+    inference_model(test_object, model)
+
+# Output:
+# Pred diffusion = SD_1.5, with probability = 0.9487
+# True diffusion = SD_1.5
 
 
 if __name__ == "__main__":
--- testing_code.py
+++ testing_code.py
@@ -1,8 +1,10 @@
 import yaml
 from ml_collections import ConfigDict
+import traceback
 
 from src.data import get_data, get_collator, create_base_transforms, transforms
-from src.model import get_model_pretraned
+from src.model import get_model_pretraned, get_inference_model
+from predict import predict_arr, inference_model
 
 
 CONFIG_TEST_RESNET = './configs/resnet_baseconf.yaml'
@@ -36,5 +38,81 @@ def test_forward(config=CONFIG_TEST_RESNET):
         model(**batch)
         print(f"Forward for {data_type} done!")
 
+def test_inference(config_path=CONFIG_TEST_RESNET):
+    with open(config_path, 'r', encoding='utf-8') as stream:
+        config = ConfigDict(yaml.safe_load(stream))
+
+    print("Load data...")
+    test_object = get_data('ExtraSmall', 'test')[617]
+    print("Load data done")
+
+    print("Creating model ...")
+    model, _ = get_model_pretraned(config.model)
+    print("Creating model done")
+
+    inference_model(test_object['png'], test_object['model.txt'], model, config_path)
+
+def test_predict(config_path=CONFIG_TEST_RESNET):
+    with open(config_path, 'r', encoding='utf-8') as stream:
+        config = ConfigDict(yaml.safe_load(stream))
+
+    print("Creating model ...")
+    model, _ = get_model_pretraned(config.model)
+    print("Creating model done")
+
+    predict_arr(model, config_path, './testing_imgs', 'testing_results.csv')
+
+def test_pretrained_model(config_path=CONFIG_TEST_RESNET):
+    test_object = get_data('ExtraSmall', 'test')[617]
+    repo_id = "3145tttt/diffusion-classification_base_resnet_50"
+    model = get_inference_model(repo_id)
+    inference_model(test_object['png'], test_object['model.txt'], model, config_path)
+
 if __name__ == "__main__":
-    test_forward()
+
+    try:
+        print("=" * 10)
+        print("Test Forward")
+        test_forward()
+    except Exception:
+        print("Test Forward error:")
+        traceback.print_exc()
+    else:
+        print("Test Forward pass")
+    print("\n\n")
+
+    try:
+        print("=" * 10)
+        print("Test inference")
+        test_inference()
+    except Exception:
+        print("Test inference error:")
+        traceback.print_exc()
+    else:
+        print("Test inference pass")
+    print("\n\n")
+    
+    try:
+        print("=" * 10)
+        print("Test predict")
+        test_predict()
+    except Exception:
+        print("Test predict error:")
+        traceback.print_exc()
+    else:
+        print("Test predict pass")
+    print("\n\n")
+
+
+    try:
+        print("=" * 10)
+        print("Test pretrained model")
+        test_pretrained_model()
+    except Exception:
+        print("Test pretrained model error:")
+        traceback.print_exc()
+    else:
+        print("Test pretrained model pass")
+    print("\n\n")
+
+
